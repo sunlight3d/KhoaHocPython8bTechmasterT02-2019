3e721ffcf3568987dafc9a64c022f07d07c395e3
Nguyen Duc Hoang <sunlight4d@gmail.com>
2019-02-22 21:41:10 +0700

    update

--- buoiso03/main.py
+++ buoiso03/main.py
@@ -1,5 +1,5 @@
 #python pip install psycopg2
-#C:\Users\Nguyen Duc Hoang\AppData\Local\Programs\Python\Python37-32\python.exe pip install psycopg2
+#C:\Users\Nguyen Duc Hoang\AppData\Local\Programs\Python\Python37-32\python.exe -m pip install psycopg2
 from database_management import DatabaseManagement
 db = DatabaseManagement()
 db.query_all()
